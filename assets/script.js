bc97b8be6fcd68b602f2fcd85cbd5cf5bc920988
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-01-29 23:01:40 -0800

    added notes for js steps

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// find dates from moment.js
+
+//create event listeners for all buttons to be clicked
+
+//create buttons for cities requested  & put in local storage
+
+//pull the local storage history and put it into an array
+
+//add the storage with the button clck 
+
+
+//how to call API?
+
+
+//API call for 5 day forecast
+
+
+
+//AJAx? will call the weather app?
+
