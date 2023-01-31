8676c42d4ed7c8ea8dc94639bf2ca0523b9f5a16
Mariah <110792371+peachysnicker@users.noreply.github.com>
2023-01-30 19:45:29 -0800

    final code added

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,96 @@
 $(document).ready(function () {
+
     var searchHistory = [];
 
-    // find dates from moment.js
-
-    //TODAYS DATE
     const momentDay = moment().format('dddd, MMMM Do');
     $('.todayDate').prepend(momentDay);
 
-//create event listeners for all buttons to be clicked
 
-//create buttons for cities requested  & put in local storage
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        let city = $('input').val();
+        if (city === '') {
 
-//pull the local storage history and put it into an array
+        }
+        call();
 
-//add the storage with the button clck 
-
-
-//how to call API?
-
-
-//API call for 5 day forecast
+    });
 
 
+    $('.searchHistoryEl').on('click', '.historyBtn', function (event) {
+        event.preventDefault();
+        let btnCityName = $(this).text();
+        call(btnCityName);
+    });
 
-//AJAx? will call the weather app
+    $('#clearBtn').on('click', function (event) {
+        window.localStorage.clear();
+        $('.searchHistoryEl').empty();
+        searchHistory = [];
+        renderButtons();
+    });
+
+    const storeCities = () =>
+        localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+
+    const renderButtons = () => {
+
+        $('.searchHistoryEl').html('');
+        for (var j = 0; j < searchHistory.length; j++) {
+            let cityName1 = searchHistory[j];
+            let historyBtn = $(
+                '<button type="button" class="btn btn-primary btn-lg btn-block historyBtn">'
+            ).text(cityName1);
+            $('.searchHistoryEl').prepend(historyBtn);
+        }
+    };
+
+
+    const init = () => {
+        let storedCities = JSON.parse(localStorage.getItem('searchHistory'));
+        if (storedCities !== null) {
+            searchHistory = storedCities;
+        }
+        renderButtons();
+    };
+
+
+
+
+
+    const call = (btnCityName) => {
+        let cityName = btnCityName || $('input').val();
+        let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=77cb488591d883bec900753d1136d81c`;
+        $.ajax({
+            url: queryURL,
+            method: 'GET',
+        })
+            .then(function (response) {
+                if (!btnCityName) {
+                    searchHistory.unshift(cityName);
+                    storeCities();
+                    renderButtons();
+                }
+
+                var lon = response.coord.lon;
+                var lat = response.coord.lat;
+
+                $('#cityName').text(response.name);
+                $('#currentImg').attr(
+                    'src',
+                    `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`
+                );
+                $('#tempData').html(`${response.main.temp} &#8457;`);
+                $('#humidData').html(`${response.main.humidity}%`);
+                $('#windData').html(`${response.wind.speed} mph`);
+                $('#windArrow').css({
+                    transform: `rotate(${response.wind.deg}deg)`,
+                });
+            })
+
+    };
+
+    call(searchHistory[0]);
+});
+
+init();
